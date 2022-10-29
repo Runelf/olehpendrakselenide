9b6a4779487705bc5e0832b575610fd839f09574
Runelf <runelf.bushi@gmail.com>
2022-10-30 00:44:23 +0300

    SelenideTest.NBA - 002 add tests

--- src/test/java/seleniumtest/appleinsider/AppleTest.java
+++ src/test/java/seleniumtest/appleinsider/AppleTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class AppleTest extends BaseTest {
 
     private final static String BASE_URL = "https://appleinsider.ru/";
-    private final static String SEARCH_STRING = "Чем iPhone 13 отличается от iPhone 12";
+    private final static String SEARCH_STRING = "Р§РµРј iPhone 13 РѕС‚Р»РёС‡Р°РµС‚СЃСЏ РѕС‚ iPhone 12";
     private final static String EXPECTED_WORD = "iphone-13";
 
     @Test
